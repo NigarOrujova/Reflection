459b794d7868b358f02d7a586a25630e42bcd5d8
Nigar Orucova <92165280+Norucova@users.noreply.github.com>
2022-12-14 02:42:29 +0400

    initial commit

---
+++ Reflection/Reflection/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public string GetValue(string propName)
+        {
+            try
+            {
+                return this.GetType().GetProperty(propName).GetValue(this,null).ToString();
+            }
+            catch
+            {
+                return "This property does not exist";
+            }
+        }
+    }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,9 @@
+﻿using Reflection.Models;
+
+User t = new User()
+{
+    UserName = "Nigar",
+    Email = "tu201906017@code.edu.az"
+};
+
+Console.WriteLine("Username: " + t.GetValue("UserName") + "\nEmail: " + t.GetValue("Email"));
