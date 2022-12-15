040210b2521d3384dc2e84e2459d7b812456ac49
Nigar Orucova <92165280+Norucova@users.noreply.github.com>
2022-12-15 18:16:53 +0400

    feat: Added Invoke

--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -1,9 +1,31 @@
 ﻿using Reflection.Models;
+using System.Reflection;
 
-User newUser = new User()
+public class Program
 {
-    UserName = "Nigar",
-    Email = "tu201906017@code.edu.az"
-};
+    public static void Main()
+    {
 
-Console.WriteLine("Username: " + newUser.GetValue("UserName") + "\nEmail: " + newUser.GetValue("Email"));
+        User newUser = new User()
+        {
+            UserName = "Nigar",
+            Email = "tu201906017@code.edu.az"
+        };
+
+
+        static void PrintValue(object obj,params object[] parameters)
+        {
+            Type type = obj.GetType();
+            MethodInfo methodInfo = type.GetMethod("GetValue");
+            Console.WriteLine(methodInfo.Invoke(obj, parameters));
+        }
+
+        PrintValue(newUser,"Email");
+        Console.WriteLine("Username: " + newUser.GetValue("UserName") + "\nEmail: " + newUser.GetValue("Email"));
+    }
+
+}
+
+
+
+
